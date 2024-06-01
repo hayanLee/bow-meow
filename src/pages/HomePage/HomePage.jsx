23ef13c8221bcfc02fa99ad4ca07abfe6f28e85a
USERMIC-Q8MSLVD\Administrator <sssb2046@gmail.com>
2024-06-01 23:00:25 +0900

    update: 업버튼 추가

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { SlArrowUpCircle } from "react-icons/sl";
+import styled from 'styled-components';
+
+const StUpIcon = styled(SlArrowUpCircle)`
+    font-size: 2rem;
+    cursor: pointer;
+    `
+
 
 export default function HomePage() {
-    return <div>HomePage: main</div>;
+    return (
+      <>
+       
+       <div>HomePage: main</div>;
+       <StUpIcon />
+      </> 
+    ) 
+    
 }
