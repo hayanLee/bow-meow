4f0201f44305e58a504d4a32f07a6f987bf79bbb
computer-note <planomenes@gmail.com>
2024-06-01 21:13:47 +0900

    ✨ update : MyPage 레이아웃 작성
    팀에서 사용하는 컬러 적용 (+개발동안 구분용 컬러 적용)
    대략적인 크기및높이 비율 적용 (이후 팀에서 세부사항 결정에 따라 수정 필요)
    
    이슈 및 제안
      - body에서 페이지 레이아웃을 가운데로 정렬해주면 보기에 더 좋을 것 같음
      - 현재 공용 버튼 및 라벨 컴포넌트가 없어서 임시로 만들어서 사용했음
      - MyPage용으로 여러개의 컴포넌트가 생길 것 같은데 전부 components 폴더에 작성하는지 아니면 components 폴더에 MyPage 폴더를 만들어서 따로 모을지 결정필요할듯

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,7 +1,166 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const gap = '10px';
+const padding = '10px';
+
+//Todo: 크기 관련 이후 팀결정에 맞춰 수정
+//  padding, gap 크기
+//  화면 대비 높이 비율
+//  브레이크 포인트
+const Main = styled.main`
+  height: 80vh;
+  min-width: 1200px;
+  max-width: 1500px;
+  display: flex;
+  justify-content: center;
+  gap: ${gap};
+  padding: ${padding};
+
+  background-color: var(--bg-color);
+  background-color: green; //개발용 컬러
+
+  color: var(--text-color);
+`;
+
+const LeftSection = styled.section`
+  width: 25%;
+
+  display: flex;
+  flex-direction: column;
+  gap: ${gap};
+
+  padding: ${padding};
+
+  background-color: palegreen; //개발용 컬러
+
+  //프로필 사진 영역
+  & > div {
+    height: 33%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background-color: skyblue; //개발용 컬러
+  }
+
+  //회원정보 수정, 회원탈퇴 버튼 영역
+  & > nav {
+    background-color: var(--box-color);
+    height: 66%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+
+    padding: ${padding};
+  }
+`;
+
+const RightSection = styled.section`
+  width: 75%;
+
+  display: flex;
+  flex-direction: column;
+  gap: ${gap};
+  padding: ${padding};
+
+  background-color: palevioletred; //개발용 컬러
+
+  & > div {
+    background-color: var(--box-color);
+  }
+
+  & > div:nth-child(2) {
+    height: 85%;
+  }
+`;
+
+const SummaryContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  gap: ${gap};
+  padding: ${padding};
+  height: 15%;
+
+  & > Label {
+    width: 33%;
+  }
+`;
+
+const PostContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  gap: ${gap};
+  padding: ${padding};
+  height: 15%;
+`;
+
+/* 
+    --bg-color: #fffef5;
+    --header-color: #bc6c25;
+    --box-color: #dda15e;
+    --point-color: #606c38;
+    --button-color: #283618;
+    --text-color: #fefae0;
+*/
+
+const Post = styled.article`
+  background-color: var(--bg-color);
+  color: black;
+  border-radius: 30px;
+  padding: 0px 10px;
+`;
+
+//공용컴포넌트 Button이 만들어지지 않아서
+//임시로 만들어 썼습니다
+const Button = styled.button`
+  background-color: var(--point-color);
+  color: var(--text-color);
+
+  border-radius: 30px;
+`;
+
+const Label = styled.label`
+  border-radius: 30px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: var(--point-color);
+  color: var(--text-color);
+`;
 
 function MyPage() {
-    return <div>MyPage</div>;
+  return (
+    <Main>
+      <LeftSection>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <nav>
+          <Button>회원정보 수정</Button>
+          <Button>회원탈퇴</Button>
+        </nav>
+      </LeftSection>
+      <RightSection>
+        <SummaryContainer>
+          <Label> 작성한 게시물 수 </Label>
+          <Label> 좋아요 수 </Label>
+        </SummaryContainer>
+        <div>
+          <p> 작성한 게시글</p>
+          <PostContainer>
+            <Post>제목: 가나다 내용: 가나다라마바사 </Post>
+            <Post>제목: 가나다 내용: 가나다라마바사 </Post>
+          </PostContainer>
+        </div>
+      </RightSection>
+    </Main>
+  );
 }
 
 export default MyPage;
