ab4af4abbd29ad9c2e6a188d0eeeb0b81b53971a
parksumi <parksumi0127@gmail.com>
2024-06-01 23:04:33 +0900

    add: HomePage.styles폴더생성

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { SlArrowUpCircle } from "react-icons/sl";
-import styled from 'styled-components';
-
-const StUpIcon = styled(SlArrowUpCircle)`
-    font-size: 2rem;
-    cursor: pointer;
-    `
-
+import { StUpIcon } from './HomePage.styles/HomePage.styled';
 
 export default function HomePage() {
-    return (
-      <>
-       
-       <div>HomePage: main</div>;
-       <StUpIcon />
-      </> 
-    ) 
-    
+  return (
+    <>
+      <div>HomePage: main</div>;
+      <StUpIcon />
+    </>
+  );
 }
