f9e77d2b5bbfe737c1a41b165fe863c870f7cd4c
USERMIC-Q8MSLVD\Administrator <sssb2046@gmail.com>
2024-06-01 23:07:03 +0900

    add: 파일 생성

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
-import { SlArrowUpCircle } from "react-icons/sl";
+import { SlArrowUpCircle } from 'react-icons/sl';
 import styled from 'styled-components';
 
 const StUpIcon = styled(SlArrowUpCircle)`
-    font-size: 2rem;
-    cursor: pointer;
-    `
-
+  font-size: 2rem;
+  cursor: pointer;
+`;
 
 export default function HomePage() {
-    return (
-      <>
-       
-       <div>HomePage: main</div>;
-       <StUpIcon />
-      </> 
-    ) 
-    
+  return (
+    <>
+      <div>HomePage: main</div>;
+      <StUpIcon />
+    </>
+  );
 }
