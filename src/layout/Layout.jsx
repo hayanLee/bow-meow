b12152b8ea2fec3f7fa63db82b7400e1f76f96cb
USERMIC-Q8MSLVD\Administrator <sssb2046@gmail.com>
2024-06-03 22:17:16 +0900

    update: layout grid 생성

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { LayoutContainer } from './Layout.styled';
 import Header from '../components/Header'
+import ImageMasonry from './Masonry';
+
+
 
 function Layout() {
   return (
     <div>
       <Header />
       <LayoutContainer>
+        <ImageMasonry />
         <Outlet />
       </LayoutContainer>
       
