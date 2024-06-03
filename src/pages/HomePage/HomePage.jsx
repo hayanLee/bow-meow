db8f79c700b82a8c5910b43a25d706c29b8208fb
hayanLee <hayanlee.dev@gmail.com>
2024-06-04 01:06:34 +0900

    add : posts FakeData 생성

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import UpButton from '../../components/common/UpButton/UpButton';
-
+import fetchPosts from '../../mockdatas/postFn';
 export default function HomePage() {
+  useEffect(() => {
+    fetchPosts().then((posts) => {
+      console.log(posts);
+    });
+  }, []);
   return (
     <>
       <div>HomePage: main</div>
