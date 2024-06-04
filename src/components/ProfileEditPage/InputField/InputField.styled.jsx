98d0a8220a013142ca6c3c0f61b6b72ca1f7423e
computer-note <planomenes@gmail.com>
2024-06-04 14:51:11 +0900

    ✨ update : ProfileEditPage 스타일 적용

--- src/components/ProfileEditPage/InputField/InputField.styled.jsx
+++ src/components/ProfileEditPage/InputField/InputField.styled.jsx
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
 
 const StInputField = styled.input`
-  width: 66%;
+  width: 80%;
+  border: 2px solid var(--point-color);
+  border-radius: 30px;
+  padding: 20px 20px;
+  outline: none;
+  font-size: 1.5rem;
 
+  &:focus {
+    outline: 1px solid var(--bg-color);
+  }
   color: black;
 `;
 
