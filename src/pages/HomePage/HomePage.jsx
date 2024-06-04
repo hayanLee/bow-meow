0e0352a1e6f19198acfd12be64d439e328f5d824
hayanLee <hayanlee.dev@gmail.com>
2024-06-04 09:14:42 +0900

    update : homepage fake data 불러오기 수정

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import UpButton from '../../components/common/UpButton/UpButton';
 import fetchPosts from '../../mockdatas/postFn';
+
 export default function HomePage() {
+  const [posts, setPosts] = useState([]);
   useEffect(() => {
     fetchPosts().then((posts) => {
-      console.log(posts);
+      setPosts(posts);
     });
   }, []);
+
+  console.log(posts);
   return (
     <>
       <div>HomePage: main</div>
