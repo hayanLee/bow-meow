8112729ae86e4fbe91eca3ae454f871021589523
USERMIC-Q8MSLVD\Administrator <sssb2046@gmail.com>
2024-06-05 19:49:29 +0900

    fix: pull 전

--- src/mockdatas/postFn.js
+++ src/mockdatas/postFn.js
@@ -1,4 +1,4 @@
-import supabase from '../supabase/supabaseClient';
+// import supabase from '../supabase/supabaseClient';
 
 export default async function fetchPosts() {
   // const { data } = await supabase.from('images').select('img_url');
