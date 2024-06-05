a1f630c9214470b2c0e5351c66408fd3f7d3d831
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-06-05 16:22:24 +0900

    update: PostDetail UI 완료(버튼 제외)

--- src/pages/PostDetailPage/PostDetailPage.jsx
+++ src/pages/PostDetailPage/PostDetailPage.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-
-import ErrorPage from '../ErrorPage';
+// 스와이퍼 관련
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination } from 'swiper/modules';
 import {
   StyledBtnContainer,
   StyledDeleteBtn,
@@ -23,34 +27,44 @@ function PostDetailPage() {
   const posts = useSelector((state) => state.posts.posts);
   const post = posts.find((s) => s.postId.toString() === postId);
 
-  if (!post) {
-    return <ErrorPage />;
-  }
-
   return (
-    <StyledDetailContainer>
+    <>
       <StyledBtnContainer>
         <StyledEditBtn>수정</StyledEditBtn>
         <StyledDeleteBtn>삭제</StyledDeleteBtn>
       </StyledBtnContainer>
-      <StyledDetailLeft>
-        <StyledDetailImg>
-          {post.images && post.images.length > 0 ? (
-            post.images.map((image, index) => <img key={index} src={image} alt={`Post Image ${index}`} />)
-          ) : (
-            <div>이미지가 없습니다</div>
-          )}
-        </StyledDetailImg>
-        <StyledDetailUser>User ID: {post.userId}</StyledDetailUser>
-      </StyledDetailLeft>
-      <StyledDetailRight>
-        <StyledRightUp>
-          <StyledDetailTitle>{post.title}</StyledDetailTitle>
-          <StyledDetailDate>{post.createdAt}</StyledDetailDate>
-        </StyledRightUp>
-        <StyledDetailPageContent>{post.content}</StyledDetailPageContent>
-      </StyledDetailRight>
-    </StyledDetailContainer>
+      <StyledDetailContainer>
+        <StyledDetailLeft>
+          <StyledDetailImg>
+            {post.images && post.images.length > 0 ? (
+              <Swiper
+                modules={[Navigation, Pagination]}
+                pagination={{ clickable: true }}
+                navigation
+                spaceBetween={50}
+                slidesPerView={1}
+              >
+                {post.images.map((image, index) => (
+                  <SwiperSlide key={index}>
+                    <img src={image} alt={`Post Image ${index}`} />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            ) : (
+              <div>이미지가 없습니다</div>
+            )}
+          </StyledDetailImg>
+          <StyledDetailUser>User ID: {post.userId}</StyledDetailUser>
+        </StyledDetailLeft>
+        <StyledDetailRight>
+          <StyledRightUp>
+            <StyledDetailTitle>{post.title}</StyledDetailTitle>
+            <StyledDetailDate>{post.createdAt}</StyledDetailDate>
+          </StyledRightUp>
+          <StyledDetailPageContent>{post.content}</StyledDetailPageContent>
+        </StyledDetailRight>
+      </StyledDetailContainer>
+    </>
   );
 }
 export default PostDetailPage;
