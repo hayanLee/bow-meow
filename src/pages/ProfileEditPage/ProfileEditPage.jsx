5b954f97996f48b9a13ccd55effb8e203fe81ec2
computer-note <planomenes@gmail.com>
2024-06-06 18:47:54 +0900

    ✨ update | ProfileEditPage : 현재 로그인한 유저 DB API로 읽어오기

--- src/pages/ProfileEditPage/ProfileEditPage.jsx
+++ src/pages/ProfileEditPage/ProfileEditPage.jsx
@@ -10,30 +10,40 @@ import {
   LowerSection
 } from '../../components/ProfileEditPage/ProfileEditPage.styles/ProfileEditPage.styled';
 
+//SupaBase API
+import { getUser } from './../../supabase/auth.login';
+
 //리액트 라이브러리
-import { useRef, useState } from 'react';
-import { useSelector } from 'react-redux';
-
-//더미 데이터
-const mockLoginedUser = {
-  userId: 101
-};
-
-//To-be: DB API 사용해서 현재 로그인된 유저 정보 가져오기
-function getLoginedUserData(loginedUserId) {
-  const users = useSelector((state) => state.users.users);
-
-  const loginedUser = users.find((user) => user.userId === loginedUserId);
-
-  return loginedUser;
-}
+import { useEffect, useRef, useState } from 'react';
 
 function ProfileEditPage() {
-  const [loginedUser, setLoginedUser] = useState(getLoginedUserData(mockLoginedUser.userId));
+  const [loginedUser, setLoginedUser] = useState(null);
 
   //아래 ref는 컴포넌트 설계 오류를 우회하기 위해 사용했습니다.
   const uploadedImgURLRef = useRef(null); // <= 임시방편
   const infoInputElemRef = useRef(null); // <= 임시방편
+
+  useEffect(() => {
+    async function loadLoginedUser() {
+      const loginedUser = await getUser();
+
+      if (loginedUser.user) {
+        setLoginedUser({
+          ...loginedUser.user,
+          ...loginedUser.user.user_metadata
+        });
+      }
+    }
+
+    loadLoginedUser();
+  }, []);
+
+  console.log('ㅡmongmong');
+
+  //로그인이 되지 않으면 리턴
+  if (!loginedUser) {
+    return <p>로그인을 해주세요!</p>;
+  }
 
   return (
     <Main>
