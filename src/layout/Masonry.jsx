4c90edf3ed1ab5b0632ea1ceafd20b4f60b49731
hayanLee <hayanlee.dev@gmail.com>
2024-06-06 23:10:49 +0900

    update : 로그인 여부에 따라 이미지/로그인 온오프

--- src/layout/Masonry.jsx
+++ src/layout/Masonry.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable no-unused-vars */
 import Masonry from '@mui/lab/Masonry';
 import Box from '@mui/material/Box';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled, { keyframes } from 'styled-components';
 import { useLoginModal } from '../hooks/useLoginModal';
